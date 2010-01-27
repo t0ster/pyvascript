92e3a34d43c704ea4eea645af2ac3685548184f3
Thomas Wanschik <devnull@localhost>
2010-01-27 15:54:14 +0100

    ended at the point where we have to start supporting indentations

--- grammar.py
+++ grammar.py
@@ -5,4 +5,13 @@ import os
 grammar_path = os.path.join(os.path.dirname(__file__), 'grammar.ometa')
 pyva_grammer = open(grammar_path, 'r').read()
 
-Grammar = OMeta.makeGrammar(pyva_grammar, {}))
+class Grammer(OMeta.makeGrammar(pyva_grammar, {}))):
+    def init(self):
+        self.keywords = set(("break", "case", "catch", "continue", "default",
+            "delete", "do", "else", "finally", "for", "function", "if", "in",
+            "instanceof", "new", "return", "switch", "this", "throw", "try",
+            "typeof", "var", "void", "while", "with", ))
+
+    def is_keyword(self, keyword):
+         return keyword in self.keywords
+
