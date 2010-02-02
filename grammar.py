a42042f56db3a9ab75d137bb7ed8ebfac455bdc4
Waldemar Kornewald <wkornewald>
2010-02-03 00:03:34 +0100

    added unfinished translator

--- grammar.py
+++ grammar.py
@@ -2,7 +2,16 @@ import os.path
 from pymeta.grammar import OMeta
 import os
 
-grammar_path = os.path.join(os.path.dirname(__file__), 'grammar.ometa')
-pyva_grammer = open(grammar_path, 'r').read()
+def to_string(string):
+    string = repr(string)
+    if string[0] == "'":
+        string = '"' + string[1:-1].replace('"', r'\"') + '"'
+    return string
 
-Grammar = OMeta.makeGrammar(pyva_grammar, {}))
+grammar_path = os.path.join(os.path.dirname(__file__), 'grammar.ometa')
+pyva_grammar = open(grammar_path, 'r').read()
+Grammar = OMeta.makeGrammar(pyva_grammar, {})
+
+translator_path = os.path.join(os.path.dirname(__file__), 'translator.ometa')
+pyva_translator = open(translator_path, 'r').read()
+Translator = OMeta.makeGrammar(pyva_translator, {'to_string': to_string})
