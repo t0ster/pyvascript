40344cdf8195c26f5c92d6b836853a75c403cd3f
Waldemar Kornewald <wkornewald>
2010-06-17 14:36:48 +0200

    move everythign into a nice package

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+DESCRIPTION = 'Pythonic JavaScript syntax'
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+setup(name='pyvascript',
+      packages=find_packages(),
+      author='Waldemar Kornewald',
+      url='http://www.allbuttonspressed.com/projects/pyvascript',
+      include_package_data=True,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      platforms=['any'],
+      install_requires=[],
+)
