41cf8a09924ccaf4d3cb722eb5e33f731413707d
Waldemar Kornewald <wkornewald>
2011-02-11 17:58:48 +0100

    fixed compile-pyva script

--- compile-pyva.py
+++ compile-pyva.py
@@ -8,7 +8,7 @@ if len(sys.argv) != 2:
 
 file_name = sys.argv[1].rsplit('.', 1)[0]
 
-g = Grammar.parse_source(open(sys.argv[1], 'r').read())
+g = Grammar.parse(open(sys.argv[1], 'r').read())
 
 new_file = open(file_name + '.js', 'w')
-new_file.write(Translator.parse_source(g))
+new_file.write(Translator.parse(g))
